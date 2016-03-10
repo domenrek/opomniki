97cf7217d42b8beabf1ba366bce5386840c21736
Domen Rek <dr8276@student.uni-lj.si>
2016-03-10 11:37:30 +0000

    dodajanje

--- opomniki/skripta.js
+++ opomniki/skripta.js
@@ -7,6 +7,20 @@ window.addEventListener('load', function() {
 		document.querySelector(".pokrivalo").style.visibility = "hidden";
 	}
 	document.querySelector("#prijavniGumb").addEventListener("click",izvediPrijavo);
+	
+	// dodaj opomnike
+	
+	var dodajOpomnik = function() {
+		var naziv_opomnika = document.querySelector("#naziv_opomnika").value;
+		var cas_opomnika = document.querySelector("#cas_opomnika").value;
+		document.querySelector("#naziv_opomnika").value="";
+		document.querySelector("#cas_opomnika").value="";
+		document.querySelector("#naziv_opomnika").innerHTML += "<div class = 'opomnik rob senca'><div class='naziv_opomnika'>"+naziv_opomnika+"</div><div class='cas_opomnika'>Opomnik čez <span>"+cas_opomnika+"</span>sekund.</div></div>";
+	
+	}
+	document.querySelector("#dodajGumb").addEventListener("click",dodajOpomnik);
+	
+	
 	//Posodobi opomnike
 	var posodobiOpomnike = function() {
 		var opomniki = document.querySelectorAll(".opomnik");
@@ -19,6 +33,12 @@ window.addEventListener('load', function() {
 			//TODO: 
 			// - če je čas enak 0, izpiši opozorilo "Opomnik!\n\nZadolžitev NAZIV_OPOMNIK je potekla!"
 			// - sicer zmanjšaj čas za 1 in nastavi novo vrednost v časovniku
+			if(cas ==0) {
+				var naziv_opomnika=document.querySelector(".naziv_opomnika").innerHTML;
+				alert("opomnik\n\nZadolžitev+"+naziv_opomnika+"je potekla!");
+			}else{
+				casovnik.innerHTML = cas -1;
+			}
 		}
 	}
 	setInterval(posodobiOpomnike, 1000);
